260604ced96d77eb04d1cf95115fbcdc59334c0b
JmC <66210216+Burgermench@users.noreply.github.com>
2023-08-08 19:30:51 +0100

    Rectified some comments.

--- 3WayQuickSort/src/Algorithm/quickSort3wayInts.java
+++ 3WayQuickSort/src/Algorithm/quickSort3wayInts.java
@@ -1,10 +1,9 @@
 //origin
-// By: Jo„o M. Cardoso 16/02/2022
-// Based on: Robert Sedgewick, Kevin Wayne - Algorithms, 4th Edition_ Essential Information about Algorithms and Data Structures-Addison-Wesley (2011)
+// By: Jo√£o M. Cardoso 16/02/2022
 
 //notes:
-// This is just a demo on the three-way version of quicksort for an array of ints.
-// A three-way quicksort algorithm is just an improved quicksort.
+// This is a take on the three-way version of quicksort for an array of ints.
+// The three-way quicksort algorithm is one of many possible improvements over your usual quicksort.
 // The improvement: In case there are many repeated values, it still compares them, but doesn't shift them around.
 // "It is far more efficient than the standard quicksort implementation for arrays with large numbers of duplicate keys"
 // In normal applications the generic version of this is more used, but in this version using Integers we can 
@@ -20,7 +19,7 @@ public class quickSort3wayInts {
 		sort(a, 0, a.length - 1);
 	}
 
-	//this version is for generic types
+	//this version is for primitive type only
 	private static void sort(int[] a, int lo, int hi) {
 		if (hi <= lo) return;
 		int lt = lo, i = lo+1, gt = hi, v = a[lo];
